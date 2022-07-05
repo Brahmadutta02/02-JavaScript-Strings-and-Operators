06234e39acd94d6c7aeec0be7f8c9b910eb17083
Brahmadutta02 <87225975+Brahmadutta02@users.noreply.github.com>
2022-07-05 12:44:37 +0530

    Add files via upload

---
+++ stringsoperator.js
@@ -0,0 +1,24 @@
+//  Javascript Operators
+
+var a = 5;
+var b = 10;
+console.log(a+b);
+console.log(b-a);
+console.log(a*b);
+console.log(a/b);
+console.log(a++);
+console.log(b--);
+
+// String Assignment
+
+var test = "Learning Javascript with AlmaBetter";
+console.log(test.length);
+console.log(test.slice(25,35));
+console.log(test.replace());
+console.log(test.toUpperCase());
+console.log(test.concat(" Web3.0 is future"));
+
+
+
+
+
